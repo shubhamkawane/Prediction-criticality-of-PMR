5e94a8fa883486c7ea6cc355a340719605cc31f7
Divya <divyagirase90@gmail.com>
2018-03-04 14:01:18 +0530

    divya UI commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { Browser } from 'protractor';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import { MatMenuModule, MatCardModule, MatIconModule, MatSidenavModule } from "@angular/material";
+import {MatListModule} from '@angular/material/list';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSelectModule} from '@angular/material/select'
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonToggleModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatExpansionModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSelectModule
+  ],
+
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
